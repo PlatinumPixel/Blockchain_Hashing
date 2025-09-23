403c43c584875ca3967ffdc5fa43e88cbfe5cb59
PlatinumPixel <96799498+PlatinumPixel@users.noreply.github.com>
2025-09-23 16:35:13 +0300

    hashing +- padaryta

--- hash.cpp
+++ hash.cpp
@@ -10,40 +10,36 @@ using namespace std;
 void hashing(string& input);
 
 int main(){  
-    string input ("iudhuwai8wd521");
+    string input ("iudhifojaoidaf4f6s6hdkoimack+6265sojimuwai8hduiwahdaioklmfsoihnfs4615dg1drijamkwd521");
     hashing(input);
     return 0;
 }
 
 void hashing(string& input){
 
-    int length;
-    if (input.size() < 64) length=64;
-    else length=input.size();
     int val[64];
     int j=1;
-    val[0]=pow((input[0]+2.5),input[1]);
-    cout << " " << val[0] << endl; 
-    for (int i =1; i<=length ;i++){    
-        j++;
-        cout <<  i<< " " << val[i] << endl;     
-        int* pastval;
-        if(i=64){
-            length -= 6; 
-            i = 0; 
-            pastval = &val[63];
+    val[0]=(input[0]*37)%1000/10%16;
+    int size=input.size();
+    if (size < 64){    
+        for (int i=1;i<64;i++){
+            val[i%64]=(input[i]*val[(i-1)%64]+input[i-1])%1000/10%16;
+            cout << (input[i%size]*val[(i-1)]+input[(i-1)%size])%1000/10%16 << endl;
         }
-        else pastval= &val[i-1];
-        
-        char* nextinp;
-        if (j=input.length()) nextinp = &input[0];
-        else nextinp=&input[j+1]; 
+    }
+    else {    
+        cout << "daugiau nei 64" << endl;
+        for (int i=1;i<size;i++){
+            val[i%64]=(input[i]*val[(i-1)%64]+input[i-1])%1000/10%16;
+            cout << (input[i]*val[(i-1)%64]+input[i-1])%1000/10%16 << endl;
+        }
 
-
-        val[i]=pow((input[j]+*pastval),*nextinp);
-        if(j>input.size()) j=0;
-        
     }
 
+    for (int i=0;i<64;i++){
+
+        cout << val[i] << " ";
+
+    }
 
 }
