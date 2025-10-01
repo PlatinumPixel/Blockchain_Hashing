c5c540999bf95fbe7a23d6866f6b1f5cd53b9034
PlatinumPixel <96799498+PlatinumPixel@users.noreply.github.com>
2025-10-01 17:25:58 +0300

    papildomi file options ir readme update

--- failoNusk.cpp
+++ failoNusk.cpp
@@ -27,3 +27,29 @@ void failoNusk (vector<string> &A, vector <string> &O){
     cout << "Laiko uzturko nuskaityt ir hashint" << t.elapsed() << endl;
     df.close();
 }
+
+void failoNuskBeSalt (vector<string> &A, vector <string> &O){
+    string failas;
+    setlocale(LC_ALL, "Lithuanian");
+
+    cout << "Iveskite failo pavadinima (pvz. text.txt)" << endl;
+    while(true){
+        cin >> failas;
+        if (!(std::filesystem::exists(failas))){
+            cout << "Toks failas neegzistuoja, pabandykite vel" << endl;
+            continue; 
+        }
+        break;
+    }
+    
+    string eil;
+
+    std::ifstream df(failas);
+    Timer t;
+    while(getline(df,eil)){
+        A.push_back(eil);
+        O.push_back(hashing(A.back()));
+    }
+    cout << "Laiko uzturko nuskaityt ir hashint" << t.elapsed() << endl;
+    df.close();
+}
--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ int main(){
         cout << "1 - Iveskite visus duomenis rankiniu budu(be salt generation) " << endl;
         cout << "2 - Iveskite duomenis rankniu budu(su salt generation) " << endl;
         cout << "3 - Sugeneruoti nauja duomenu faila " << endl;
-        cout << "4 - Paiimti duomenis is failo " << endl;      
-        cout << "5 - Baigti darba ir spausdinti i terminala " << endl;
-        cout << "6 - Baigti darba ir spausdinti i faila " << endl;
+        cout << "4 - Paiimti duomenis is failo (su salt generation) " << endl;      
+        cout << "5 - Paiimti duomenis is failo (be salt generation) " << endl;      
+        cout << "6 - Baigti darba ir spausdinti i terminala " << endl;
+        cout << "7 - Baigti darba ir spausdinti i faila " << endl;
 
         try {
-            if (!(cin>>input)||input<1 || input>6){
+            if (!(cin>>input)||input<1 || input>7){
                 cin.clear();
                 cin.ignore();
                 throw "Ivestas neteisingas simbolis";
@@ -51,14 +52,18 @@ int main(){
                 case 4:
                     failoNusk(I,O);
                 break;
-                
+
                 case 5:
+                    failoNuskBeSalt(I,O);
+                break;
+                
+                case 6:
                     spausdina(I,O);
                     cout << "Spauskite Enter, kad uzdaryti programa..." << endl;
                     cin.ignore();
                     cin.get();
                     return 0;
-                 case 6:
+                 case 7:
                     cout << "Iveskite failo pavadinima ";
                     cin >> failas;
                     spausdinaFaila(I,O,failas);
@@ -66,12 +71,6 @@ int main(){
                     cin.ignore();
                     cin.get();
                     return 0;
-                // case 9:
-                //     run_unit_tests();
-                //     cout << "Spauskite Enter, kad uzdaryti programa..." << endl;
-                //     cin.ignore();
-                //     cin.get();
-                //     return 0;
                 default:
                     cout << "Ivedete neteisinga simobli, pabandykit vel! :)" << endl;
                 break;
